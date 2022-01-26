167152ffca027ead739809741c472752d9be0aad
Korbi777 <kobyakov20@mail.ru>
2022-01-26 15:11:50 +0400

    Website

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
+$(function(){
+  $('.sl').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    dots: true
+  });
+
+  $('.btn_menu').on('click', function(){
+    $('.menu-list').slideToggle();
+  });
+
+});
